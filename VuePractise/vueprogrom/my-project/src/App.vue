30cc57d769db1f6213ed6b1e964f50adc0e499c1
pengzheng <1667184346@qq.com>
2018-05-14 17:41:20 +0800

    vuex项目练习以及常见的ajax请求

---
+++ VuePractise/vueprogrom/my-project/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+   <div id="app">
+   	     <h1>在线翻译</h1>
+   	     <h5 class="text-muted">简单、易用，便捷</h5>
+         <translateForm v-on:formSubmit="translateText"></translateForm>
+         <translateOutput v-text="trabslatedText"></translateOutput>
+   </div>  
+</template>
+<script>
+
+import TranslateForm from './components/TranslateForm'
+import TranslateOutput from './components/TranslateOutput'
+
+export default {
+  name:"app",
+  components:{
+  	TranslateForm,TranslateOutput 	
+  },
+  methods:{
+  	translateText:function(text,language){
+//		alert(text);
+      this.$http.get('https:translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl&lang='+language+'&text='+text)
+      .then((reponse)=>{
+      	console.log(response.body.text[0]);
+      	this.trenslatedText = respose.body.text[0];
+      })
+  	}
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ VuePractise/vueprogrom/my-project/src/components/TranslateForm.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="row" id="translateForm">
+  	<form id="transForm" v-on:submit="formSubmit">
+  		<input type="text" v-model="textToTranslate"  placeholder="请输入需要翻译的文字">
+  		<select v-model="language">
+  			<option value="en">English</option>
+  		</select>
+  		<input type="submit" value="翻译">
+  	</form>
+  </div>
+</template>
+<script>
+
+export default {
+  name:'translateForm',
+  data:function{
+  	return {
+  		textToTranslate:"",
+  		language:""
+  	}
+  },
+  methods:{
+  	formSubmit:function(e){
+//		alert(this.textToTranslate);
+  		this.$emit("formSubmit",this.textToTranslate,this.language);
+  		e.preventDefault();
+  	},
+  	created:function(){
+  		this.language = "en"
+  	}
+  }
+}
+</script>
+
+<style>
+
+</style>
